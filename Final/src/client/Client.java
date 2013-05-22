0e19824dbd4662a4f13585a926b5974e80aa1e00
Nick Ames <names144@gmail.com>
2013-05-21 21:44:51 -0700

    Added additional methods and menu bar.

--- Final/src/client/Client.java
+++ Final/src/client/Client.java
@@ -3,6 +3,7 @@ import java.awt.Cursor;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -11,11 +12,16 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import javax.swing.JMenuBar;
+import java.awt.Toolkit;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 
 public class Client {
 
-	private JFrame frame;
+	private JFrame frmTruClient;
 	private ClientProcess cp;
 
 	private Thread client;
@@ -28,7 +34,7 @@ public class Client {
 			public void run() {
 				try {
 					Client window = new Client();
-					window.frame.setVisible(true);
+					window.frmTruClient.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,20 +53,22 @@ public class Client {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 304, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmTruClient = new JFrame();
+		frmTruClient.setIconImage(Toolkit.getDefaultToolkit().getImage(Client.class.getResource("/org/jfree/chart/gorilla.jpg")));
+		frmTruClient.setTitle("T.R.U. Client | V1.0.0");
+		frmTruClient.setBounds(100, 100, 304, 300);
+		frmTruClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTruClient.getContentPane().setLayout(null);
 
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 288, 262);
-		frame.getContentPane().add(panel);
+		frmTruClient.getContentPane().add(panel);
 		panel.setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("T.R.U. Temperature Client");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 11, 268, 59);
+		lblNewLabel.setBounds(10, 21, 268, 49);
 		panel.add(lblNewLabel);
 
 		final JLabel lblStatus = new JLabel("Disconnected");
@@ -79,7 +87,7 @@ public class Client {
 					cp = new ClientProcess("localhost", 61223);
 					client = new Thread(cp);
 					try {
-						frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+						frmTruClient.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 						cp.connect();
 						client.start();
 						btnConnect.setVisible(false);
@@ -87,11 +95,11 @@ public class Client {
 						lblStatus.setForeground(Color.GREEN);
 						lblStatus.setText("Connected");
 						btnReal.setEnabled(true);
-						frame.repaint();
+						frmTruClient.repaint();
 					} catch (Exception e) {
 						System.out.println(e.getMessage());
 					} finally {
-						frame.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+						frmTruClient.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 					}
 				}
 			}
@@ -101,18 +109,18 @@ public class Client {
 		btnDisconnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+					frmTruClient.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 					cp.disconnect();
 					btnDisconnect.setVisible(false);
 					btnConnect.setVisible(true);
 					lblStatus.setForeground(Color.RED);
 					lblStatus.setText("Disconnected");
 					btnReal.setEnabled(false);
-					frame.repaint();
+					frmTruClient.repaint();
 				} catch (Exception e) {
 					System.out.println(e.getMessage());
 				} finally {
-					frame.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+					frmTruClient.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 				}
 			}
 		});
@@ -137,8 +145,65 @@ public class Client {
 		btnNewButton.setBounds(84, 188, 116, 23);
 		panel.add(btnNewButton);
 
-		JButton btnNewButton_1 = new JButton("Exit");
-		btnNewButton_1.setBounds(84, 222, 116, 23);
-		panel.add(btnNewButton_1);
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (btnDisconnect.isVisible()){
+					try {
+						cp.disconnect();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				System.exit(0);
+			}
+		});
+		btnExit.setBounds(84, 222, 116, 23);
+		panel.add(btnExit);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 288, 21);
+		panel.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mExit = new JMenuItem("Exit");
+		mExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (btnDisconnect.isVisible()){
+					try {
+						cp.disconnect();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				System.exit(0);
+			}
+		});
+		mnNewMenu.add(mExit);
+		
+		JMenu mnNewMenu_1 = new JMenu("Connection");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mPref = new JMenuItem("Preferences");
+		mnNewMenu_1.add(mPref);
+		
+		JMenu mnNewMenu_2 = new JMenu("Help");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mHow = new JMenuItem("How To");
+		mnNewMenu_2.add(mHow);
+		
+		JMenuItem mAbout = new JMenuItem("About");
+		mAbout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(frmTruClient, "Temperature Reading Unit - Client Version 1.0.0" + System.lineSeparator() 
+						+ "Nicholas Ames" + System.lineSeparator() + "Bryan Johnson");
+			}
+		});
+		mnNewMenu_2.add(mAbout);
 	}
 }
